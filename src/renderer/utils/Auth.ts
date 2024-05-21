709f1943e8f9d586831fa5cb0f66b8b4c95d33a3
SSPENELOPE <typoepping@gmail.com>
2024-05-21 15:32:23 -0400

    Added logout method, added more types to auth class for type strict variables

--- src/renderer/utils/Auth.ts
+++ src/renderer/utils/Auth.ts
@@ -1,9 +1,9 @@
 /* eslint-disable no-console */
 class AuthService {
-  static async isLoggedIn() {
-    const url = `http://localhost:3001/user/auth/isLoggedIn`;
+  static async isLoggedIn(): Promise<boolean> {
+    const url: string = `http://localhost:3001/user/auth/isLoggedIn`;
     try {
-      const response = await fetch(url, {
+      const response: Response = await fetch(url, {
         method: 'GET',
         credentials: 'include',
         headers: {
@@ -22,10 +22,26 @@ class AuthService {
   }
 
   // TODO: Make this call the backend to clear the JWT since its HTTP-Only
-  // static logout() {
-  //   Cookies.remove('JWT');
-  //   window.location.replace('/');
-  // }
+  static async logout(): Promise<boolean> {
+    const url: string = `http://localhost:3001/user/auth/logout`;
+    try {
+      const response: Response = await fetch(url, {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+          'Content-type': 'application/json',
+        },
+      });
+      if (response.status === 200) {
+        return true;
+      }
+      return false;
+    } catch (error) {
+      // throw new Error();
+      console.error('error: ', error);
+      return false;
+    }
+  }
 }
 
 export default AuthService;
